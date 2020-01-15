23fb1a4ced533648e56f8d761e184c780ca6c087
centron5961 <37583948+centron5961@users.noreply.github.com>
2020-01-15 20:13:21 +0100

    ToDo: funktionen implementieren (4)

--- organisator.cpp
+++ organisator.cpp
@@ -4,3 +4,28 @@ Organisator::Organisator()
 {
 
 }
+
+
+
+bool KlassenmitgliedBearbeiten(string vorname, string nachname, string geburtsname, string eMail, string kennwort, string telNr)
+{
+    //
+}
+
+
+bool Login(string eMail, string password)
+{
+    //
+}
+
+
+bool Logout()
+{
+    //
+}
+
+
+bool PasswortAendern(string alt, string neu)
+{
+    //
+}
