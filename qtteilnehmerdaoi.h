30ac830c49ddd8c0272be81a2ad57479f5d77078
centron5961 <37583948+centron5961@users.noreply.github.com>
2020-01-15 20:03:10 +0100

    interface nach UML fertig implementiert

---
+++ qtteilnehmerdaoi.h
@@ -0,0 +1,34 @@
+#ifndef QTTEILNEHMERDAOI_H
+#define QTTEILNEHMERDAOI_H
+
+#include <vector>
+#include <QSqlDatabase>
+#include "klassenmitglied.h"
+#include "qtteilnehmerdao.h"
+
+using namespace std;
+
+class QtTeilnehmerDaoI
+{
+public:
+    QtTeilnehmerDaoI(){}
+    virtual ~QtTeilnehmerDaoI(){}
+
+    virtual QtTeilnehmerDao Instance() = 0;
+    virtual int InsertTeilnehmer() = 0;
+    virtual int ModifyTeilnehmer() = 0;
+    virtual Klassenmitglied GetTeilnehmer() = 0;
+    virtual vector<Klassenmitglied> GetTeilnehmerListe() = 0;
+    virtual bool Attach() = 0;
+    virtual bool Detach() = 0;
+    virtual bool Notify() = 0;
+    virtual int SetPath() = 0;
+
+
+private:
+    QtTeilnehmerDao uniqueInstance;
+    vector<Klassenmitglied> teilnehmerListe;
+    QSqlDatabase database;
+};
+
+#endif // QTTEILNEHMERDAOI_H
