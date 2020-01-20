17cb272ee06c3ad3b2d7a1d41786c8be08812194
centron5961 <37583948+centron5961@users.noreply.github.com>
2020-01-20 21:05:50 +0100

    Adresse::operator ==() overloaded

--- adresse.h
+++ adresse.h
@@ -15,6 +15,7 @@ class Adresse
         std::string getPlz(){return plz;}
         std::string getLand(){return land;}
 
+        bool operator ==(const Adresse& d);
     private:
         std::string strasse;
         std::string hausnummer;
